93ad30cae7ad6933e187d67acb8b256ee4747cb3
Victor Rabay <4451622+rabay@users.noreply.github.com>
2024-11-07 11:12:04 -0300

    Criando uma nova VPC

--- main.tf
+++ main.tf
@@ -14,15 +14,31 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+# data "aws_vpc" "default" {
+#   default = true
+# }
+
+module "web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.web_sg.security_group_id]
+
+  subnet_id = module.web_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -34,7 +50,7 @@ module "web_sg" {
   version = "5.2.0"
   name    = "web"
 
-  vpc_id              = data.aws_vpc.default.id
+  vpc_id              = module.web_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
