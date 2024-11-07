74dd6eb59550ba2c1c88ce7057511fe8870c8137
Victor Rabay <4451622+rabay@users.noreply.github.com>
2024-11-06 22:35:03 -0300

    Padronizando security group

--- main.tf
+++ main.tf
@@ -29,17 +29,10 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow HTTP/HTTPS in. Allow everything out."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name    = "web_new"
+  name    = "web"
 
   vpc_id              = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
@@ -47,34 +40,3 @@ module "web_sg" {
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
-
-resource "aws_security_group_rule" "web_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
