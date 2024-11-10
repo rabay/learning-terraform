60a74b924057ba4210bc8aff3aea6f32b177091a
Victor Rabay <4451622+rabay@users.noreply.github.com>
2024-11-10 14:00:42 -0300

    Transformando valores em variáveis

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.name] # Bitnami
 }
 
 # data "aws_vpc" "default" {
@@ -21,25 +21,25 @@ data "aws_ami" "app_ami" {
 module "web_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs            = ["sa-east-1a", "sa-east-1b", "sa-east-1c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "8.0.0"
-  name    = "web"
+  name    = "${var.environment.name}-web"
 
-  min_size            = 1
-  max_size            = 2
+  min_size            = var.asg_min_size
+  max_size            = var.asg_max_size
   desired_capacity    = 1
   health_check_type   = "EC2"
   vpc_zone_identifier = module.web_vpc.public_subnets
@@ -63,7 +63,7 @@ resource "aws_autoscaling_attachment" "web-aat" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name            = "web-alb"
+  name            = "${var.environment.name}-web-alb"
   vpc_id          = module.web_vpc.vpc_id
   subnets         = module.web_vpc.public_subnets
   security_groups = module.web_sg.security_group_id
@@ -80,17 +80,16 @@ module "alb" {
 
   target_groups = {
     ex-instance = {
-      name        = "web-instances"
+      name        = "${var.environment.name}-web-instances"
       name_prefix = "web"
       protocol    = "HTTP"
       port        = 80
       target_type = "instance"
-      target_id   = aws_instance.web.id
     }
   }
 
   tags = {
-    Environment = "Development"
+    Environment = var.environment.name
     Project     = "Web"
   }
 }
@@ -98,7 +97,7 @@ module "alb" {
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name    = "web"
+  name    = "${var.environment.name}-web"
 
   vpc_id              = module.web_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
